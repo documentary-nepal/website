830dc12a5e66288bf0a5732a6ec8d5711e292d30
aashu10sh <pudasainiashutosh@gmail.com>
2025-10-01 14:30:29 +0545

    added: about, contact and service pages

--- src/lib/components/landing/hero.svelte
+++ src/lib/components/landing/hero.svelte
@@ -3,7 +3,7 @@
 	<img src="/image/b1.jpg" loading="lazy" alt="Mountain Yak in a hill." class="" />
 	<div class="text-center position-absolute">
 		<!-- <h2 class="text-small font-light text-[#fff] mt-8 mb-4 uppercase">We are</h2> -->
-		<h1 class="md:text-7xl text-4xl font-bold"  id="demo">Documentary Nepal</h1>
+		<h1 class="md:text-7xl text-4xl font-bold" id="demo">Documentary Nepal</h1>
 		<p class="my-8 md:text-3l text-l font-semibold text-[#fff]">
 			At Documentary Nepal, we bring untold stories of culture, history, and people to life. Through
 			powerful films and authentic storytelling, we aim to preserve Nepal's voice and share it with
--- src/routes/service/+page.svelte
+++ src/routes/service/+page.svelte
@@ -1,1 +1,195 @@
-<h1>Service Page</h1>
+<section class="w-80">
+	<div class="grid-2 sasfg">
+		<div class="margin-top-304 mr-8">
+			<h2 class="text-semi uppercase">Our Services</h2>
+			<p class="my-5 text-[#273339]">
+				We are able to fixed projects, from simple location scouting, to complete film production
+				facilities and service, including scouting, casting, location permits, crew and equipment,
+				hotel booking, travel and a lot more.
+			</p>
+			<a href="/contact">
+				<button
+					class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-blue-700 hover:border-blue-500 rounded"
+				>
+					Collaborate with us
+				</button>
+			</a>
+		</div>
+		<div class="side-illust">
+			<img src="/image/bcd1.jpeg" alt="" />
+		</div>
+	</div>
+
+	<div class="my-36">
+		<h2 class="text-bg mb-10 oaujlkju">We are willing to offer you:</h2>
+		<div class="grid-3 gap-3">
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-map text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Fixer</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p class="text-2">
+						Documentary Nepal has the best fixers in Nepal. It is important to hire a fixer when
+						filming in a foreign country because a fixer can help facilitate the logistics of
+						filming in a foreign country and act as a liaison between the film crew and the local
+						community. Our fixers can help navigate local customs and cultural norms to ensure that
+						the film crew is respectful and sensitive to the local community.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">01.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-users text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Filming Crew</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						All our team members have hundreds of years in combined experience, and our team has
+						been working with Documentary Nepal for more than 10 years on average. Our filming crew
+						& team members are specialists in their own chosen sectors and would be more than glad
+						to provide and give advice while filming in Nepal.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">02.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-book text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Filming Permits</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						It is necessary for you to acquire the “National Filming Permit” which grants you the
+						permission to film on non-private or non-special locations. Obtaining filming permits is
+						an important step in the production process when filming in a foreign country, helping
+						to ensure that the film crew is in compliance with local laws and regulations, and that
+						the necessary precautions are taken to ensure the safety and well-being of all involved.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">03.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-clapperboard text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Skilled Line Producers</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						All of our dedicated line producers are highly skilled and have very sound knowledge of
+						the process of film making. They play a crucial role in ensuring that a film is produced
+						efficiently and within budget. They are also responsible for creating and managing the
+						production schedule, ensuring that all necessary tasks are completed on time and on
+						budget.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">04.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-car text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Transportation</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						Transportation services can help to ensure that the film crew has reliable and
+						convenient access to the locations where they will be filming.Transportation services
+						can help to ensure the safety of the film crew, especially when filming in areas that
+						may not be familiar to them. Professional drivers who are familiar with local conditions
+						and routes can help to ensure that the film crew arrives at their destinations safely
+						and efficiently.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">05.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-location-dot text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Location Scouting</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						Nepal is a country with a wide range of landscapes, from the Himalayan mountains to
+						subtropical forests, and location scouting is necessary to identify suitable locations
+						for filming that meet the specific needs of the production.Location scouting also helps
+						to assess the logistics and safety of filming in a particular location. This can include
+						identifying any potential challenges such as access to power and water, as well as any
+						potential risks such as unstable terrain or inclement weather.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">06.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-4">
+					<i class="fa-solid fa-utensils text-3xl mt-1"></i>
+					<h2 class="font-bolder text-2xl">Food</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						Providing food for the film crew is essential for ensuring that they have access to
+						nutritious meals that will help them stay healthy and energized during long and
+						demanding days of filming.Providing food on set can also help to increase efficiency by
+						minimizing the need for the film crew to take breaks to go out and find food. This can
+						help to keep the production on schedule and within budget. Our team is properly equipped
+						to handle catering/food services for all types of teams, Big or Small.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">07.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-phone text-3xl mt-1"></i>
+					<h2 class="font-bold text-2xl">Proper Communication</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						All our team members are fluent in English and some of them are also familiar with other
+						foreign languages. Proper communication is essential for overcoming these barriers and
+						ensuring that the film crew is able to communicate effectively with locals.Proper
+						communication is essential for coordinating the various tasks and activities involved in
+						film production, such as location scouting, casting, and rehearsals. Without effective
+						communication, it can be difficult to ensure that everyone is on the same page and
+						working towards the same goals.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">08.</p>
+			</div>
+			<!-- #endregion -->
+			<!-- #region -->
+			<div class="s1 border border-solid border-[#273339] p-5">
+				<div class="flex gap-5">
+					<i class="fa-solid fa-camera text-3xl mt-1"></i>
+					<h2 class="font-bold text-2xl">Film Equipment Rental</h2>
+				</div>
+				<div class="innercontent-services my-5">
+					<p>
+						We have a large database of filming equipment in Nepal, Film equipment may not always be
+						readily available in every country, and renting equipment can ensure that the film crew
+						has access to the necessary tools and technologies to produce their film. film equipment
+						rental is an important resource for filmmakers when filming in a foreign nation like
+						Nepal, as it helps to ensure that they have access to the necessary tools and
+						technologies to produce their film in an efficient and cost-effective manner.
+					</p>
+				</div>
+				<p class="text-5xl text-[#ccc]" style="font-weight: 900">09.</p>
+			</div>
+			<!-- #endregion -->
+		</div>
+	</div>
+</section>
